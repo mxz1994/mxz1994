209ac2a98bd851a4d9ab18295ff94eaa6bb15229
maxingzheng <maxingzheng@qq.com>
2020-09-28 16:40:50 +0800

    init

---
+++ math/math.md
@@ -0,0 +1,3 @@
+# 剑指
+
+[03. 两数之和](./src/main/java/com/jianzhi/A03.java)
---
+++ math/src/main/java/com/jianzhi/A03.java
@@ -0,0 +1,19 @@
+package com.jianzhi;
+
+/**
+ *
+ * 在一个长度为 n 的数组里的所有数字都在 0 到 n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字是重复的，也不知道每个数字重复几次。请找出数组中任意一个重复的数字。
+ *
+ * Input:
+ * {2, 3, 1, 0, 2, 5}
+ *
+ * Output:
+ * 2
+ *
+ * @author mxz on 2020/09/28 16:38
+ */
+public class A03 {
+    public static void main(String[] args) {
+
+    }
+}
